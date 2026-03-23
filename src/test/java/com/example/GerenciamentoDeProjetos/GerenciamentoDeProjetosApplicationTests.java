ec921dc0139fa65216648b208d36470467d1d33d
Heitor Ferrari <heitorferrari9@gmail.com>
2026-03-22 21:28:12 -0300

    commit inicial

---
+++ src/test/java/com/example/GerenciamentoDeProjetos/GerenciamentoDeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GerenciamentoDeProjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
